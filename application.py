5e85891fc0940842f5e86cc7cb6062da9c50918b
Monica Chang <mychang07@gmail.com>
2019-12-04 04:23:52 +0000

    delete extraneous functions

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology
 
 # testing git
 
