d2d5dce33b5ecc53a2907714f1722401392dfc37
pkumar23 <54913928+pkumar23@users.noreply.github.com>
2019-12-05 21:35:27 -0500

    Update popup.py

--- templates/popup.py
+++ templates/popup.py
@@ -1,25 +1,8 @@
-def friends():
+def popup(location):
     """Allow user to follow their friends and see their current location in a table"""
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        username=request.form.get("username") # search for friend's username
-
-        # Ensure username is entered
-        if not username:
-            return apology("must provide username", 403)
-        else:
-            # Query database for username
+    if request.method == "CLICK":
             rows = db.execute("SELECT * FROM users WHERE username = :username", username=username)
-            return apology("TODO")
-        # Ensure username exists before following friend
-        if len(rows) != 1:
-            return apology("invalid username" , 403)
-        else:
-            return apology("TODO")
-            #TODO update queries to establish follower/followee relationship
+        return redirect("/maps")
 
-        return redirect("/friends")
-
-    else:
-        # query to get your friends and their current locations
-        return render_template("friends.html", userinfo=session.get("userinfo"))
+  
