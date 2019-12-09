25c6909c4e1e1e218b86ac3dbaac41ea71a1a468
pkumar23 <54913928+pkumar23@users.noreply.github.com>
2019-12-08 21:05:19 -0500

    Delete popup.py

--- templates/popup.py
+++
@@ -1,8 +0,0 @@
-@app.route("/check", methods=["GET", "POST"])
-def popup(loc):
-    """Allow user to follow their friends and see their current location in a table"""
-    if request.method == "POST":
-            rows = db.execute("SELECT numpeople FROM locations WHERE location = :loc", locations=loc)
-        return redirect("/maps")
-
-  
